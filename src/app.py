1aba0d373f38f649673786a986529934d6840042
Nick Hsiao <hsiao.nicholas@gmail.com>
2022-04-17 00:02:08 -0700

    home route

--- src/app.py
+++ src/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return "Hello! this is the main page <h1>Hello<h1>"
 
+@app.route("/<name>")
+def user(name):
+    return f"Hello {name}!"
+    
 if __name__ == "__main__":
     app.run()
