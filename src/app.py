b9f8915377b56feb07a32c2a284a1112cd63d02c
Nick Hsiao <hsiao.nicholas@gmail.com>
2022-04-20 12:11:46 -0700

    clean up

--- src/app.py
+++ src/app.py
@@ -6,14 +6,6 @@ app = Flask(__name__)
 def home(name):
     return render_template("index.html",content=name)
 
-"""
-@app.route("/<name>")
-def user(name):
-    return f"Hello {name}!"
 
-@app.route("/admin/")
-def admin():
-    return redirect(url_for("user",name="admin"))
-"""
 if __name__ == "__main__":
     app.run()
